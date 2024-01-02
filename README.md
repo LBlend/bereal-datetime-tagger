17216d3eecb30ea29b265bd0682cd3b4d3cae3c5
LBlend <24893890+LBlend@users.noreply.github.com>
2024-01-02 17:35:30 +0100

    add timezone support

--- README.md
+++ README.md
@@ -8,17 +8,30 @@ Well I have. So I wrote this script to do the work for me
 
 You need to request a copy of your data from BeReal. The images themselves don't contain the metadata however it is located in the json files that come along with it.
 
+Make sure that the copy of your data has been unzipped
+
 ## Installation
 
-TBA
+```
+python -m pip install -r requirements.txt -U
+```
 
 ## Usage
 
+```
+python main.py -i <input_directory> -o <output_directory> [-t <timezone>}
+```
+
+Timezone name must be among the `pytz`'s supported names. Your safest bet though is using the `<Region/City>` format. See example below.
+
 ### A quick warning
 
-~~Note that if you do not use the timezone parameter, the program will use your system's local time. This also means that BeReals taken in another timezone or several timezones will not be respected.~~ This feature is not even implemented yet, thus it will make use of UTC for now
+* All images will be tagged using the same timezone. If no timezone is provided, UTC time will be used.
+
+* Directories provided when running the program is relative to the location of the main.py file.
 
 ### Example
 
-TBA
-
+```
+python main.py -i ../my-unzipped-bereal-data -o ../tagged-bereal-images -t Asia/Seoul
+```
--- main.py
+++ main.py
@@ -1,42 +1,73 @@
+import argparse
 import json
 import os
 import sys
 from datetime import datetime
 
-import dateutil
+import dateutil.parser
 import exif
+import pytz
 from PIL import Image
 
 
 def main():
-    # Parse command line arguments
-    # I should probably use a lib to do flags but whatever
-    sys.argv = sys.argv[1:]
-    if len(sys.argv) != 2 or len(sys.argv) != 3:
-        print("Usage: python main.py <input_directory> <output_directory>")
-        sys.exit(1)
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Yeet")
+    parser.add_argument(
+        "-i", "--input_directory", help="The root folder of your BeReal data"
+    )
+    parser.add_argument(
+        "-o", "--output_directory", help="The folder to export your tagged files to"
+    )
+    parser.add_argument(
+        "-t",
+        "--timezone",
+        help="The timezone in in which images should be tagged. Defaults to UTC",
+        required=False,
+    )
 
-    input_directory = sys.argv[0]
-    output_directory = sys.argv[1]
+    argument = parser.parse_args()
+    input_directory = argument.input_directory
+    output_directory = argument.output_directory
+
+    if not argument.timezone:
+        timezone = pytz.utc
+        print("No timezone provided! Using UTC time...")
+    else:
+        try:
+            timezone = pytz.timezone(argument.timezone)
+        except pytz.exceptions.UnknonTimeZoneError:
+            print("Invalid timezone")
+            sys.exit(1)
 
     # Check if input directory exists
     print("Reading metadata...")
-    with open(f"{input_directory}/memories.json", "r") as f:
-        memories = json.load(f)
+    try:
+        with open(f"{input_directory}/memories.json", "r") as f:
+            memories = json.load(f)
+    except FileNotFoundError:
+        print(
+            "The provided input directory does not contain a memories.json metadata file"
+        )
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print("Failed to read memories.json metadata file.")
+        sys.exit(1)
 
     # Create output directory
     os.makedirs(output_directory, exist_ok=True)
 
+    # Process bereals
     for memory in memories:
         front_image = correct_path(memory["frontImage"]["path"], input_directory)
         print(f"Processing {front_image}")
         front_image = convert_to_jpg(front_image, output_directory)
-        set_metadata(front_image, memory["takenTime"])
+        set_metadata(front_image, memory["takenTime"], timezone)
 
         back_image = correct_path(memory["backImage"]["path"], input_directory)
         print(f"Processing {back_image}")
         back_image = convert_to_jpg(back_image, output_directory)
-        set_metadata(back_image, memory["takenTime"])
+        set_metadata(back_image, memory["takenTime"], timezone)
 
     print("Done!")
 
@@ -67,8 +98,9 @@ def get_filename(path: str) -> str:
     return path.split("/")[-1].split(".")[0]
 
 
-def set_metadata(image_path: str, date: str):
-    date = dateutil.parser.parse(date).strftime("%Y:%m:%d %H:%M:%S")
+def set_metadata(image_path: str, date: str, timezone):
+    date = dateutil.parser.parse(date)
+    date = date.astimezone(timezone).strftime("%Y:%m:%d %H:%M:%S")
 
     with open(image_path, "rb") as image_file:
         my_image = exif.Image(image_file)
